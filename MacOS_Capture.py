6814103809b5ee365385c3515abecd138e33b174
Sumeth <sumeth.nav@gmail.com>
2026-04-15 14:59:08 +0700

    Refactor window capture functionality into WindowCapture class

--- MacOS_Capture.py
+++ MacOS_Capture.py
@@ -1,18 +1,35 @@
 import Quartz
 import numpy as np
 
-def capture_window(window_id):
-    image = Quartz.CGWindowListCreateImage(
-        Quartz.CGRectNull,
-        Quartz.kCGWindowListOptionIncludingWindow,
-        window_id,
-        Quartz.kCGWindowImageDefault
-    )
+class WindowCapture:
+    def __init__(self, window_name):
+        # find the handle for the window we want to capture
+        self.hwnd = win32gui.FindWindow(None, window_name)
+        if not self.hwnd:
+            raise Exception('Window not found: {}'.format(window_name))
 
-    width = Quartz.CGImageGetWidth(image)
-    height = Quartz.CGImageGetHeight(image)
+    def screenshot(self):
+        image = Quartz.CGWindowListCreateImage(
+            Quartz.CGRectNull,
+            Quartz.kCGWindowListOptionIncludingWindow,
+            self.hwnd,
+            Quartz.kCGWindowImageDefault
+        )
 
-    data = Quartz.CGDataProviderCopyData(Quartz.CGImageGetDataProvider(image))
-    img = np.frombuffer(data, dtype=np.uint8).reshape((height, width, 4))
+        width = Quartz.CGImageGetWidth(image)
+        height = Quartz.CGImageGetHeight(image)
 
-    return img[:, :, :3]
+        data = Quartz.CGDataProviderCopyData(Quartz.CGImageGetDataProvider(image))
+        img = np.frombuffer(data, dtype=np.uint8).reshape((height, width, 4))
+        return img[:, :, :3]
+    
+    def findwindowsid(self):
+        windows = Quartz.CGWindowListCopyWindowInfo(
+            Quartz.kCGWindowListOptionOnScreenOnly,
+            Quartz.kCGNullWindowID
+        )
+
+        windows_name = ""
+        for w in windows:
+            # print(w.get('kCGWindowNumber'), w.get('kCGWindowOwnerName'), w.get('kCGWindowName'))
+            windows_name = w.get('kCGWindowNumber')
